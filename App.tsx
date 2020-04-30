a158496068dcf02d6695d16a9830a92384483538
Zafer AYAN <ozcanzaferayan@gmail.com>
2020-04-30 21:35:56 +0300

    Add App.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,53 @@
 import React from 'react';
 import LottieView from 'lottie-react-native';
-const App = () => (
-  <LottieView
-    source={require('./animations/16445-photo-files-upload.json')}
-    autoPlay
-    loop
-  />
-);
+import {SafeAreaView, StatusBar, StyleSheet, Text, Button} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.safeAreaView}>
+        <Text style={styles.title}>Dosyalarızı saklayın</Text>
+        <Text style={styles.subtitle}>
+          Dosyalarınızı en iyi şifreleme yöntemleri ile güvenli bir şekilde
+          bulut ağında saklayabilirsiniz.
+        </Text>
+        <LottieView
+          style={styles.lottieView}
+          source={require('./animations/16445-photo-files-upload.json')}
+          autoPlay
+          loop
+        />
+        <Button
+          title="Yüklemeye başla"
+          onPress={() => console.log('clicked')}
+        />
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    margin: 22,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    marginTop: 8,
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#666',
+    marginBottom: -50,
+  },
+  lottieView: {
+    height: 300,
+  },
+});
+
 export default App;
